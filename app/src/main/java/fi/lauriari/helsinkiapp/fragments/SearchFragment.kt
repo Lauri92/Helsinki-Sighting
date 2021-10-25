3c46e79560a07fd2a2dc1465f29fa478d3be6f69
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-10-26 01:59:39 +0300

    optimized search fragment, request to api lifted to it's own function

--- app/src/main/java/fi/lauriari/helsinkiapp/fragments/SearchFragment.kt
+++ app/src/main/java/fi/lauriari/helsinkiapp/fragments/SearchFragment.kt
@@ -17,7 +17,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import fi.lauriari.helsinkiapp.MainActivity
 import fi.lauriari.helsinkiapp.R
 import fi.lauriari.helsinkiapp.adapters.ItemsAdapter
-import fi.lauriari.helsinkiapp.adapters.SearchAdapter
 import fi.lauriari.helsinkiapp.classes.SingleHelsinkiItem
 import fi.lauriari.helsinkiapp.databinding.FragmentSearchBinding
 import fi.lauriari.helsinkiapp.viewmodels.HelsinkiApiViewModel
@@ -27,16 +26,17 @@ import kotlinx.coroutines.launch
 import android.app.Activity
 
 import android.view.inputmethod.InputMethodManager
-
-
-
-
+import android.widget.Button
+import androidx.core.view.forEach
+import androidx.core.view.forEachIndexed
 
 class SearchFragment : Fragment() {
 
     lateinit var binding: FragmentSearchBinding
     private val apiViewModel: HelsinkiApiViewModel by viewModels()
     private val searchAdapter: ItemsAdapter by lazy { ItemsAdapter("searchFragment") }
+    private lateinit var layoutManager: LinearLayoutManager
+    private var firstLoad: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,9 +48,10 @@ class SearchFragment : Fragment() {
         initializeViewModelRepositoryBinding(binding)
         initNavigation()
         initSearchViewQueryTextListener()
+        setButtons()
 
-
-        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerview.layoutManager = layoutManager
         binding.recyclerview.adapter = searchAdapter
 
 
@@ -61,68 +62,17 @@ class SearchFragment : Fragment() {
         binding.searchview.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 query ?: return false
-
-                val imm =
-                    activity!!.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-                var view = activity!!.currentFocus
-                if (view == null) {
-                    view = View(activity)
-                }
-                imm.hideSoftInputFromWindow(view.windowToken, 0)
-
-                val adapterList = mutableListOf<SingleHelsinkiItem>()
-                lifecycleScope.launch(Dispatchers.IO) {
-                    val getActivities = async {
-                        apiViewModel.getActivities(query, "en")
-                    }
-                    getActivities.await().let {
-                        if (it.isSuccessful && it.body()!!.data.isNotEmpty()) {
-                            val createList = async {
-                                it.body()?.data?.forEach { info ->
-                                    adapterList.add(
-                                        SingleHelsinkiItem(
-                                            id = info.id,
-                                            name = info.name.en,
-                                            infoUrl = info.info_url,
-                                            latitude = info.location.lat,
-                                            longitude = info.location.lon,
-                                            streetAddress = info.location.address.street_address,
-                                            locality = info.location.address.locality,
-                                            description = info.description.body,
-                                            images = info.description.images,
-                                            tags = info.tags,
-                                            whereWhenDuration = info.where_when_duration
-                                        )
-                                    )
-                                }
-                            }
-                            createList.await()
-                            activity?.runOnUiThread {
-                                searchAdapter.setData(adapterList)
-                                binding.recyclerview.visibility = View.VISIBLE
-                            }
-                        } else {
-                            activity?.runOnUiThread {
-                                Toast.makeText(
-                                    requireContext(),
-                                    "Nothing was found!",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                binding.recyclerview.visibility = View.GONE
-                            }
-                        }
-                    }
-                }
+                getItems(query)
                 return true
             }
 
             override fun onQueryTextChange(query: String?): Boolean {
                 query ?: return false
-
                 if (query == "") {
+                    binding.buttonsContainer.visibility = View.VISIBLE
                     binding.recyclerview.visibility = View.GONE
+                    layoutManager.scrollToPositionWithOffset(0, 0)
                 }
-
                 return true
             }
         })
@@ -155,6 +105,101 @@ class SearchFragment : Fragment() {
     private fun initializeViewModelRepositoryBinding(binding: FragmentSearchBinding) {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewmodel = apiViewModel
+        Log.d("firstload", firstLoad.toString())
+        if (firstLoad) {
+            binding.recyclerview.visibility = View.GONE
+            binding.buttonsContainer.visibility = View.VISIBLE
+            firstLoad = false
+        } else {
+            Log.d("firstload", "in else block!!")
+            binding.recyclerview.invalidate()
+            binding.recyclerview.visibility = View.VISIBLE
+            binding.buttonsContainer.invalidate()
+            binding.buttonsContainer.visibility = View.GONE
+        }
+    }
+
+    private fun getItems(text: String) {
+        val imm =
+            activity?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        var view = activity?.currentFocus
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+        val adapterList = mutableListOf<SingleHelsinkiItem>()
+        lifecycleScope.launch(Dispatchers.IO) {
+            val getActivities = async {
+                apiViewModel.getActivities(text, "en")
+            }
+            getActivities.await().let {
+                if (it.isSuccessful && it.body()!!.data.isNotEmpty()) {
+                    val createList = async {
+                        it.body()?.data?.forEach { info ->
+                            adapterList.add(
+                                SingleHelsinkiItem(
+                                    id = info.id,
+                                    name = info.name.en,
+                                    infoUrl = info.info_url,
+                                    latitude = info.location.lat,
+                                    longitude = info.location.lon,
+                                    streetAddress = info.location.address.street_address,
+                                    locality = info.location.address.locality,
+                                    description = info.description.body,
+                                    images = info.description.images,
+                                    tags = info.tags,
+                                    whereWhenDuration = info.where_when_duration
+                                )
+                            )
+                        }
+                    }
+                    createList.await()
+                    activity?.runOnUiThread {
+                        searchAdapter.setData(adapterList)
+                        binding.buttonsContainer.visibility = View.GONE
+                        binding.recyclerview.visibility = View.VISIBLE
+                    }
+                } else {
+                    activity?.runOnUiThread {
+                        Toast.makeText(
+                            requireContext(),
+                            "Nothing was found!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        binding.recyclerview.visibility = View.GONE
+                        binding.buttonsContainer.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setButtons() {
+        val activitiesList = listOf(
+            "Sauna",
+            "Boating",
+            "Ecological",
+            "National park",
+            "Food",
+            "Archipelago",
+            "History",
+            "Music",
+            "Trips",
+            "Sea"
+        )
+        binding.buttonsContainer.forEachIndexed { index, view ->
+            setSingleButtonTextAndListener(view as Button, activitiesList[index])
+        }
+    }
+
+    private fun setSingleButtonTextAndListener(button: Button, textValue: String) {
+        val textLowerCase = textValue.lowercase()
+        button.text = textValue
+        button.setOnClickListener {
+            binding.searchview.setQuery(textLowerCase, false)
+            getItems(textLowerCase)
+        }
     }
 
     override fun onDestroyView() {
